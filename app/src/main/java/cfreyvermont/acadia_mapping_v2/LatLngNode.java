ccb7eb6ba0dd3be6ed3dc51a8f8ad782d975e247
Christian Frey <cfreyvermont@gmail.com>
2015-12-30 16:05:34 -0500

    Created a class to represent nodes in the graph.

---
+++ app/src/main/java/cfreyvermont/acadia_mapping_v2/LatLngNode.java
@@ -0,0 +1,74 @@
+package cfreyvermont.acadia_mapping_v2;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Christian on 12/30/2015.
+ * The nodes for the graph providing directions will be an extension of
+ * a LatLng point, and also includes if a node is a building, and what
+ * building it is.
+ */
+public class LatLngNode {
+    public LatLng latLng;
+    public final boolean isEndNode;
+    public final String buildingCode;
+
+
+    /**
+     * Creates a vertex where the point is not a building node.
+     * @param latLng the position of the vertex
+     */
+    public LatLngNode(LatLng latLng) {
+        this(latLng, false, "");
+    }
+
+    /**
+     * Creates a vertex.
+     * @param latLng The position of the vertex
+     * @param isEndNode If the vertex is a building
+     * @param code the building code. null if vertex is not a building.
+     */
+    public LatLngNode(LatLng latLng, boolean isEndNode, String code) {
+        this.latLng = latLng;
+        this.isEndNode = isEndNode;
+        if (isEndNode) {
+            this.buildingCode = code;
+        } else {
+            this.buildingCode = "";
+        }
+    }
+
+    /**
+     * Checks if two vertexes are equal. Uses the .equals function from the
+     * LatLng library.
+     * @param node the node you want to compare.
+     * @return true if the nodes are equal, false if not.
+     */
+    public boolean isVertexEqual(LatLngNode node) {
+        if (node != null) {
+            boolean equalPoints = this.latLng.equals(node.latLng);
+            boolean equalEndNode = this.buildingCode.equals(node.buildingCode);
+            boolean equalBuildingCode = (this.isEndNode == node.isEndNode);
+
+            return equalPoints && equalEndNode && equalBuildingCode;
+        }
+        return false;
+    }
+
+    /**
+     * Overriding the toString method gives us a human-readable version of
+     * all of the contents of the object, for debugging purposes.
+     * @return A string containing the details of the object.
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.latLng.toString());
+        if (!isEndNode) {
+            return sb.toString();
+        }
+        sb.append(", isEndNode: ").append(this.isEndNode);
+        sb.append(", BuildingCode:").append(this.buildingCode);
+        return sb.toString();
+    }
+}
---
+++ app/src/test/java/cfreyvermont/acadia_mapping_v2/LatLngNodeTest.java
@@ -0,0 +1,28 @@
+package cfreyvermont.acadia_mapping_v2;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Christian on 12/30/2015.
+ *
+ * Tests the LatLngNode class and methods.
+ */
+public class LatLngNodeTest extends TestCase {
+    private LatLngNode n1 = new LatLngNode(new LatLng(45, 45));
+    private LatLngNode n2 = new LatLngNode(new LatLng(45, 45), true, "CIO");
+    private LatLngNode n3 = new LatLngNode(new LatLng(55, 55), true, "CIO");
+    private LatLngNode n4 = new LatLngNode(new LatLng(55, 55), true, "CIO");
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testIsVertexEqual() throws Exception {
+        assertEquals(false, n1.isVertexEqual(n2));
+        assertEquals(false, n2.isVertexEqual(n3));
+        assertEquals(true, n3.isVertexEqual(n4));
+    }
+}
